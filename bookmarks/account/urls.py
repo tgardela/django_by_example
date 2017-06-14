a611b00c79ae114d522b0dca49184b648f30a477
tgardela <king_nothin@o2.pl>
2017-06-14 08:46:11 +0200

    added profile editing options

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -28,6 +28,8 @@ urlpatterns = [
 
     url(r'^register/$', views.register, name='register'),
 
+    url(r'^edit/$', views.edit, name='edit'),
+
     url(r'^$', views.dashboard, name='dashboard'),
 
 ]
